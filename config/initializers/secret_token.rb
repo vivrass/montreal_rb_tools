1a455abde82a512c48169f3ea2ecdc6a7c4ecd3b
Martin Provencher <mprovencher86@gmail.com>
2011-11-16 19:53:56 -0500

    Rails new - Here we go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MontrealRbTools::Application.config.secret_token = '0ef0846daa369380b787ccd54c5f59b42e6f66521a9598da5985cff47e994341de9d91183f528790c9fcfd3b15a98202ead04347b3e351af3ed68f16aa185001'
