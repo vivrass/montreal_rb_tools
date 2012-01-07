cc445e99fcd000cebc3da42d16dd6c4b0087282a
Martin Provencher <mprovencher86@gmail.com>
2012-01-07 15:03:48 -0500

    Implement sign_(in,out,up) links)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 MontrealRbTools::Application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get   "sign_in"  => "devise/sessions#new", :as => :sign_in
+    get   "sign_out" => "devise/sessions#destroy", :as => :sign_out
+    get   "sign_up" => "devise/registrations#new", :as => :sign_up
+  end
 
   resources :talks, :only => [:index, :new, :create]
 
